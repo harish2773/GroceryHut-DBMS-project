73dc263a1a7f90cde5012f5e10b27fc922df24fb
Harish Dendukuri <98586849+harish2773@users.noreply.github.com>
2023-04-24 18:53:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
+import { SearchBar } from "./components/searchbar";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="logo">
+        
+      </div>
+      <ShopContextProvider>
+      
+        <Router>
+          <Navbar />
+          <SearchBar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+        
+      </ShopContextProvider>
+
+      
+    </div>
+  );
+}
+
+export default App;
